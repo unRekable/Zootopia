ec2005e9f12fd4e51a2e8acda4ac372bf7057937
FelixG <Felix@driveby.com>
2025-05-13 15:01:43 +0200

    Added a function to read the html template. Then, replacing the placeholder with the actual animal information and saving it to a new animals.html

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -7,8 +7,20 @@ def load_data(file_path):
 
 animals_data = load_data('animals_data.json')
 
+with open('animals_template.html', 'r') as outfile:
+    html_template = outfile.read()
+
+output = ''
+
 for animal in animals_data:
-    print(f"Name: {animal['name']}")
-    print(f"Diet: {animal['characteristics'].get('diet')}")
-    print(f"Location: {animal['locations'][0]}")
-    print(f"Type: {animal['characteristics'].get('type')}")
+    output += f"Name: {animal['name']}\n"
+    output += f"Diet: {animal['characteristics'].get('diet')}\n"
+    output += f"Location: {animal['locations'][0]}\n"
+    if animal['characteristics'].get('type'):
+        output += f"Type: {animal['characteristics'].get('type')}\n"
+    output += f"\n"
+
+template = html_template.replace("__REPLACE_ANIMALS_INFO__", output)
+
+with open('animals.html', 'w') as outfile:
+    outfile.write(template)
